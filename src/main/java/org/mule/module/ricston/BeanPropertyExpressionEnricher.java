990737267fd09237240be5060cefa6cd82b1c314
Claude Mamo <claude.mamo@ricston.com>
2011-11-28 23:28:32 +0100

    Added licenses; tidying up; ignored test

--- src/main/java/org/mule/module/ricston/BeanPropertyExpressionEnricher.java
+++ src/main/java/org/mule/module/ricston/BeanPropertyExpressionEnricher.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Ricston Ltd.  All rights reserved.  http://www.ricston.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.module.ricston;
 
 import org.mule.api.MuleMessage;
