22198ab886a3fba5563a7e87ee582d9f66eeedb0
Claude Mamo <claude.mamo@ricston.com>
2012-01-12 15:19:04 +0100

    Added parsers; WIP

---
+++ src/main/java/org/mule/module/ricston/DefaultExceptionProcessorChainBuilder.java
@@ -0,0 +1,11 @@
+package org.mule.module.ricston;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: claudemamo
+ * Date: 11/01/2012
+ * Time: 17:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class DefaultExceptionProcessorChainBuilder {
+}
---
+++ src/main/java/org/mule/module/ricston/ExceptionProcessorChainFactoryBean.java
@@ -0,0 +1,11 @@
+package org.mule.module.ricston;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: claudemamo
+ * Date: 11/01/2012
+ * Time: 17:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExceptionProcessorChainFactoryBean {
+}
